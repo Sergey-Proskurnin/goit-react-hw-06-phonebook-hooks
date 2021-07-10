3562de0365385a792232bed06ecea1ea665ccc21
Sergey-Proskurnin <proskurnin@ua.fm>
2021-07-11 02:11:31 +0300

    Delete comments

--- src/redux/contacts/contacts-reducer.js
+++ src/redux/contacts/contacts-reducer.js
@@ -4,54 +4,6 @@ import initialContacts from 'components/localStorage';
 import { createReducer } from '@reduxjs/toolkit';
 import contactsAction from './contacts-actions';
 
-// ----------------first-step-------------------
-// import types from './contacts-types';
-// import contactsTest from 'data/contactsTest.json';
-// import parsedContacts from 'components/localStorage';
-// import stateItemNoRepeat from 'components/helpersReducer';
-
-// const items = (
-//   state = parsedContacts ? parsedContacts : contactsTest,
-//   { type, payload },
-// ) => {
-//   switch (type) {
-//     case types.ADD:
-//       if (
-//         state.some(
-//           ({ name }) => name.toLowerCase() === payload.name.toLowerCase(),
-//         )
-//       ) {
-//         alert(`${payload.name} is already in contacts`);
-//         return [...state];
-//       }
-//       if (state.some(({ number }) => number === payload.number)) {
-//         alert(`${payload.number} is already in contacts`);
-//         return [...state];
-//       }
-
-//       return [payload, ...state];
-
-// // stateItemNoRepeat(state, action),
-
-//     case types.DELETE:
-//       return state.filter(({ id }) => id !== payload);
-//     default:
-//       return state;
-//   }
-// };
-
-// const filter = (state = '', { type, payload }) => {
-//   switch (type) {
-//     case types.CHANGE_FILTER:
-//       return payload;
-//     default:
-//       return state;
-//   }
-// };
-//----------------------------------------------------------------------
-
-// ---------------second-step--------------------------------------------
-
 const items = createReducer(initialContacts, {
   [contactsAction.addContact]: (state, action) => [action.payload, ...state],
   [contactsAction.deleteContact]: (state, action) =>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,17 +1,6 @@
 import contactsReducer from './contacts/contacts-reducer';
 import { configureStore } from '@reduxjs/toolkit';
 
-// ----------------first-step-------------------
-// import { createStore, combineReducers } from 'redux';
-// import { composeWithDevTools } from 'redux-devtools-extension';
-// const rootReducer = combineReducers({
-//   contacts: contactsReducer,
-// });
-
-// const store = createStore(rootReducer, composeWithDevTools());
-//----------------------------------------------------------------------
-
-// ---------------second-step--------------------------------------------
 const store = configureStore({
   reducer: {
     contacts: contactsReducer,
