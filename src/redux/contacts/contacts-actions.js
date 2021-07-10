8c7e41644e82cc850da8e1db68ebc207f0c670f8
Sergey-Proskurnin <proskurnin@ua.fm>
2021-07-11 01:53:50 +0300

    Test

--- src/redux/contacts/contacts-actions.js
+++ src/redux/contacts/contacts-actions.js
@@ -1,36 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
 import { createAction } from '@reduxjs/toolkit';
 
-// ----------------first-step-------------------
-// import types from './contacts-types';
-
-// const addContact = ({ name, number }) => ({
-//   type: types.ADD,
-//   payload: {
-//     name,
-//     number,
-//     id: uuidv4(),
-//   },
-// });
-
-// const deleteContact = contactId => ({
-//   type: types.DELETE,
-//   payload: contactId,
-// });
-
-// const changeFilter = value => ({
-//   type: types.CHANGE_FILTER,
-//   payload: value,
-// });
-//--------------------------------------------------------------------------------
-
-// ---------------second-step-----------------------------------------------------
 const addContact = createAction('contacts/add', ({ name, number }) => ({
   payload: {
     name,
     number,
     id: uuidv4(),
-  },
+  ,
 }));
 const deleteContact = createAction('contacts/delete');
 const changeFilter = createAction('contacts/changeFilter');
